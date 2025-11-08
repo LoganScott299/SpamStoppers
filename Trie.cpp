b28d9297db50586c33110db5e3140f4f9545d6c2
shrav <smandava1@ufl.edu>
2025-11-08 13:01:41 -0500

    trie fix

--- Trie.cpp
+++ Trie.cpp
@@ -6,71 +6,70 @@
 
 //trie node class
 class node{
-  public:
-    //indicates that spam/phrase or wrd is complete/reached its end
-    bool is_blacklisted_word;
-    //contains 128 nodes to represent ASCII character set
-    node* children[128];
+public:
+  //indicates that spam/phrase or wrd is complete/reached its end
+  bool is_blacklisted_word;
+  //contains 128 nodes to represent ASCII character set
+  node* children[128];
 
-    //constructor for trie node
-    node(){
-        is_blacklisted_word = false;
-        for(node* c : children){
-            c = nullptr;
-        }
+  //constructor for trie node
+  node(){
+    is_blacklisted_word = false;
+    for(node* c : children){
+      c = nullptr;
     }
+  }
 };
 
 //trie class
 class trie {
 
-  trie* root;
+  node* root;
 
-  public:
-     trie(){
-       root = new node();
-     }
+public:
+  trie(){
+    root = new node();
+  }
 
-     //insert word into trie
-     void insert(std::string word){
-       node* curr = root;
-       for(char c : word){
-         int index = static_cast<int>(c);
-         if(!curr->children[index]){
-           curr->children[index] = new node();
-         }
-         curr = curr->children[index];
-       }
-       curr->is_blacklisted_word = true;
-     }
+  //insert word into trie
+  void insert(std::string word){
+    node* curr = root;
+    for(char c : word){
+      int index = static_cast<int>(c);
+      if(!curr->children[index]){
+        curr->children[index] = new node();
+      }
+      curr = curr->children[index];
+    }
+    curr->is_blacklisted_word = true;
+  }
 
-     //search for word in trie
-     bool search(std::string word){
-       node* curr = root;
+  //search for word in trie
+  bool search(std::string word){
+    node* curr = root;
 
-       for(char c : word){
-         int index = static_cast<int>(c);
-         if(!curr->children[index]){
-           return false;
-         }
-         curr = curr->children[index];
-       }
-       return curr->is_blacklisted_word;
-     }
+    for(char c : word){
+      int index = static_cast<int>(c);
+      if(!curr->children[index]){
+        return false;
+      }
+      curr = curr->children[index];
+    }
+    return curr->is_blacklisted_word;
+  }
 
-     //delete word from trie
-     void remove(std::string word){
-       node* curr = root;
+  //delete word from trie
+  void remove(std::string word){
+    node* curr = root;
 
-       for(char c : word){
-         int index = static_cast<int>(c);
-         if(!curr->children[index]){
-           return;
-         }
-         curr = curr->children[index];
-       }
-       if(curr->is_blacklisted_word == true){
-         curr->is_blacklisted_word = false;
-       }
-     }
-
+    for(char c : word){
+      int index = static_cast<int>(c);
+      if(!curr->children[index]){
+        return;
+      }
+      curr = curr->children[index];
+    }
+    if(curr->is_blacklisted_word == true){
+      curr->is_blacklisted_word = false;
+    }
+  }
