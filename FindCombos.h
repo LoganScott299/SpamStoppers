82e12e17c63834adf86129c8c42af24a4e965ca2
LoganScott299 <logan.scott.official@gmail.com>
2025-11-07 20:46:56 -0500

    Add findTop10 function to find the 10 largest values in an unordered_map
    This function receives an unordered map of string, int pairs.
    
    It sorts them and finds the 10 largest values using a minHeap.
    
    It outputs a vector containing 10 <string, int> pairs starting with the largest.

--- FindCombos.h
+++ FindCombos.h
@@ -6,6 +6,9 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <unordered_map>
+#include <queue>
+#include <algorithm>
 
 // Split string of words into all up to n word sequential combinations
 std::vector<std::string> up_to_n_combos(const std::string& str, int n) {
@@ -32,3 +35,32 @@ std::vector<std::string> up_to_n_combos(const std::string& str, int n) {
     }
     return result;
 }
+
+// Find the top 10 string, int pairs in an unordered map
+std::vector<std::pair<std::string,int>> findTop10(const std::unordered_map<std::string,int>& map) {
+    std::vector<std::pair<std::string,int>> top10;
+    // Make a minHeap of int, string pairs
+    // int comes first so that it compares the values, not the keys
+    std::priority_queue<std::pair<int, std::string>,
+        std::vector<std::pair<int, std::string>>,
+        std::greater<std::pair<int, std::string>>> minHeap;
+
+    // Push values from our unordered map into the minHeap
+    // and pop the smallest value when size > 10
+    for (auto& pair : map) {
+        minHeap.push({pair.second, pair.first});
+        if (minHeap.size() > 10) minHeap.pop();
+    }
+
+    //Push the top 10 largest values into a vector
+    while (!minHeap.empty()) {
+        std::pair<int, std::string> top = minHeap.top();
+        top10.push_back({top.second, top.first});
+        minHeap.pop();
+    }
+
+    // Reverse the vector so it starts with the largest value (#1, #2, #3...)
+    std::reverse(top10.begin(), top10.end());
+    return top10;
+}
+
